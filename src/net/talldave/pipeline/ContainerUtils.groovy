917084868e1de09454f4419d4255bd3ef071fcca
David Kuster <dave@talldave.net>
2017-12-05 16:10:48 -0700

    added python and kubectl containers

--- src/net/talldave/pipeline/ContainerUtils.groovy
+++ src/net/talldave/pipeline/ContainerUtils.groovy
@@ -13,19 +13,63 @@ package net.talldave.pipeline
  * mavenContainer(image: 'xyz')
  * mavenContainer(name: 'abc', image: 'xyz')
  */
-def mavenContainer(String name = Defaults.mavenName,
-                   String image = Defaults.mavenImage) {
+def mavenContainer(String name = Defaults.mavenContainerName,
+                   String image = Defaults.mavenContainerImage) {
     echo "creating Maven container with name [$name] and image [$image]"
+    genericContainer(
+        name,
+        image,
+        [
+            containerEnvVar(
+                key: 'MAVEN_OPTS',
+                value: '-Duser.home=/root/')
+        ]
+    )
+}
+
+
+/**
+ * Creates a Python container.
+ *
+ * Usage:
+ *
+ * pythonContainer()
+ * pythonContainer(name: 'abc')
+ * pythonContainer(image: 'xyz')
+ * pythonContainer(name: 'abc', image: 'xyz')
+ */
+def pythonContainer(String name = Defaults.pythonContainerName,
+                   String image = Defaults.pythonContainerImage) {
+    echo "creating Python container with name [$name] and image [$image]"
+    genericContainer(name, image)
+}
+
+
+/**
+ * Creates a container with Kubectl.
+ *
+ * Usage:
+ *
+ * kubectlContainer()
+ * kubectlContainer(name: 'abc')
+ * kubectlContainer(image: 'xyz')
+ * kubectlContainer(name: 'abc', image: 'xyz')
+ */
+def kubectlContainer(String name = Defaults.kubectlContainerName,
+                   String image = Defaults.kubectlContainerIamge) {
+    echo "creating Kubectl container with name [$name] and image [$image]"
+    genericContainer(name, image)
+}
+
+
+// utility method to DRY out container templates
+def genericContainer(String name, String image, List envVars = []) {
     containerTemplate(
         name: name,
         image: image,
         command: '/bin/sh -c',
         args: 'cat',
         ttyEnabled: true,
-        envVars: [
-            containerEnvVar(
-                key: 'MAVEN_OPTS',
-                value: '-Duser.home=/root/')
-        ]
+        envVars: envVars
     )
 }
--- src/net/talldave/pipeline/Defaults.groovy
+++ src/net/talldave/pipeline/Defaults.groovy
@@ -2,7 +2,13 @@ package net.talldave.pipeline
 
 class Defaults {
 
-    static final mavenName = 'maven'
-    static final mavenImage = 'fabric8/maven-builder:2.2.331'
+    static final mavenContainerName = 'maven'
+    static final mavenContainerImage = 'fabric8/maven-builder:2.2.331'
+
+    static final pythonContainerName = 'python'
+    static final pythonContainerImage = 'internal image - must be set'
+
+    static final kubectlContainerName = 'kubectl'
+    static final kubectlContainerImage = 'fabric8/builder-clients:v5366c66'
 
 }
