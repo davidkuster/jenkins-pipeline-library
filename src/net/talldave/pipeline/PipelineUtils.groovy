a36f9a32a36626d50d8816bc94ce509a58707872
David Kuster <dave@talldave.net>
2017-12-05 14:09:24 -0700

    add getNodeLabel() method, comments

--- src/net/talldave/pipeline/PipelineUtils.groovy
+++ src/net/talldave/pipeline/PipelineUtils.groovy
@@ -1,9 +1,21 @@
 package net.talldave.pipeline
 
-String getVersion() {
+/**
+ * Create the label to use for the build agent pod
+ */
+String getNodeLabel() {
+    "buildpod.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_').replace('%2F', '_').replace('%', '_')
+}
+
+/**
+ * Create the version value to use for the current build.
+ * Includes the Git SHA, build number and timestamp.
+ * Example: commit_79be90b_build_62_20171205205148
+ */
+String getBuildVersion() {
     String cmd = 'git rev-parse --short HEAD'
     String commit_id = sh(script: cmd, returnStdout: true).trim()
     String version = "commit_${commit_id}_build_${env.BUILD_NUMBER}_${env.BUILD_TIMESTAMP}"
     echo "using version $version"
-    return version
+    version
 }
